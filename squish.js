4b050055358eddb81563ee408b40dbe02c03602a
Dean Edwards <dean.edwards@gmail.com>
2008-01-07 19:06:34 +0000

    Added support for the "squish" module.

---
+++ squish.js
@@ -0,0 +1,2 @@
+/* IE7/IE8.js - copyright 2004-2008, Dean Edwards */
+if(IE7.appVersion<7){IE7.CSS.addFix(/float\s*:\s*(left|right)/,"display:inline;$1");if(IE7.appVersion>=6)IE7.CSS.addRecalc("float","left|right",function(element){IE7.Layout.boxSizing(element.parentElement);element.style.display="inline"});IE7.CSS.addRecalc("position","absolute|fixed",function(a){if(a.offsetParent&&a.offsetParent.currentStyle.position=="relative")IE7.Layout.boxSizing(a.offsetParent)})}
